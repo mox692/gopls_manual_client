06a9c1dd2f55cbd862b90f41c7d4dc789733cf12
mox692 <55653825+mox692@users.noreply.github.com>
2021-06-12 14:32:10 +0900

    fix

--- client/request_textDocument_didChange.go
+++ client/request_textDocument_didChange.go
@@ -10,17 +10,18 @@ func (req *ClientRequester) didChange() error {
 	var didChangeResult interface{}
 	// TODO: ここのparamsは別所から取ってくるようにする
 
-	didChangeParams := protocol.DidChangeTextDocumentParams{
-		TextDocument: protocol.VersionedTextDocumentIdentifier{
-			Version: 2,
-			TextDocumentIdentifier: protocol.TextDocumentIdentifier{
-				URI: "dammyURI",
-			},
-		},
-	}
 	err := req.conn.Call(context.Background(), "textDocument/didChange", didChangeParams, &didChangeResult)
 	if err != nil {
 		return err
 	}
 	return nil
+}
+
+var didChangeParams = protocol.DidChangeTextDocumentParams{
+	TextDocument: protocol.VersionedTextDocumentIdentifier{
+		Version: 2,
+		TextDocumentIdentifier: protocol.TextDocumentIdentifier{
+			URI: "dummyURI",
+		},
+	},
 }
